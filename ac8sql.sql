3ad6b986b3b153ed9b5ea0c85aaed8685be24a15
Erica Ribeiro <54157468+Erica-Ribeiro@users.noreply.github.com>
2019-10-07 21:44:59 -0300

    Update ac8sql.sql

--- ac8sql.sql
+++ ac8sql.sql
@@ -1,49 +1,55 @@
 CREATE TABLE Usuario(
 	id INT NOT NULL,
-	registro UNIQUE NOT NULL,
+	registro VARCHAR NOT NULL,
 	nome VARCHAR (120) NOT NULL,
-	email UNIQUE VARCHAR (50) NOT NULL,
+	email  VARCHAR (50) NOT NULL,
 	celular INT NOT NULL,
-	login UNIQUE VARCHAR (30) NOT NULL,
+	login VARCHAR (30) NOT NULL,
 	senha VARCHAR (50) NOT NULL,
-	data_expiracao DATE NOT NULL
-	CONSTRAINT pkUsuario PRIMARY KEY (id)
+	data_expiracao DATE NOT NULL CONSTRAINT ('01/01/1900')
+	CONSTRAINT pkUsuario PRIMARY KEY (id),
+	CONSTRAINT UQ_registro UNIQUE(registro),
+	CONSTRAINT UQ_email UNIQUE(email),
+	CONSTRAINT UQ_login UNIQUE(login)
 	);
 
 CREATE TABLE Professor(
 	id INT NOT NULL,
-	apelido VARCHAR(50) NOT NULL,
 	id_usuario INT NOT NULL,
-	CONSTRAINT fk_Professor_Usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+	apelido VARCHAR(50) NOT NULL,
+	CONSTRAINT fk_Professor_Usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id),
 	CONSTRAINT pkProfessor PRIMARY KEY (id)
 	);
 
 CREATE TABLE Coordenador(
 	id INT NOT NULL,
 	id_usuario INT NOT NULL,
-	CONSTRAINT fk_Professor_Usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+	CONSTRAINT fk_Professor_Usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id),
 	CONSTRAINT pkCoordenador PRIMARY KEY (id)
 );
 
 CREATE TABLE Curso(
 	id INT NOT NULL,
-	nome UNIQUE VARCHAR(120) NOT NULL,
+	nome VARCHAR(120) NOT NULL,
 	sigla VARCHAR(5) UNIQUE NOT NULL,
 	tipo VARCHAR (30) NOT NULL,
 	duracao TINYINT NOT NULL,
 	sobre VARCHAR (150) NOT NULL,
 	id_coordenador INT NOT NULL,
-	CONSTRAINT fk_curso_coordenador FOREIGN KEY (id_coordenador) REFERENCES Coordenador(id)
+	CONSTRAINT UQ_nome UNIQUE(nome),
+	CONSTRAINT fk_curso_coordenador FOREIGN KEY (id_coordenador) REFERENCES Coordenador(id),
 	CONSTRAINT pkCurso PRIMARY KEY (id)
 );
 	
 CREATE TABLE Turma(
 	id INT NOT NULL,
-	id_curso UNIQUE INT NOT NULL,
-	semestre UNIQUE TINYINT NOT NULL,
+	id_curso INT NOT NULL,
+	semestre TINYINT NOT NULL,
 	letra UNIQUE VARCHAR (2) NOT NULL,
 	periodo VARCHAR (15) NOT NULL,
-	CONSTRAINT fk_turma_curso FOREIGN KEY (id_curso) REFERENCES Curso(id)
+	CONSTRAINT UQ_Curso UNIQUE(Curso),
+	CONSTRAINT UQ_semestre UNIQUE(semestre),
+	CONSTRAINT fk_turma_curso FOREIGN KEY (id_curso) REFERENCES Curso(id),
 	CONSTRAINT pkTurma PRIMARY KEY (id)
 	
 );
